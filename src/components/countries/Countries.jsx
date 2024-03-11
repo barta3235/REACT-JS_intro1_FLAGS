81b6e0627b1923f6630b2d5e2e349831f36c7acb
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-03-11 17:55:59 +0600

    css rendering with updating state in arrays and pass parameter

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -5,18 +5,36 @@ import './countries.css'
 
 const Countries = () => {
     const [countries,setCountries]= useState([]);
+    const [visitedCountries,setVisitedCountries]=useState([]);
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=> res.json())
         .then(data=>setCountries(data));
     },[])
+
+
+    const handleVisitedCountry=(country)=>{
+        const newVisitedCountries=[...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries);
+        // console.log(newVisitedCountries);
+    }
+
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                   {
+                       visitedCountries.map((country)=> <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country-container">
             {
-                 countries.map((country)=> <Country key={country.car.cca3} country={country}></Country> )
+                 countries.map((country)=> <Country key={country.car.cca3} handleVisitedCountry={handleVisitedCountry} country={country}></Country> )
             }
             </div>
         </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountry}) => {
     const {name,flags,population,area,cca3}=country
-    console.log(country);
+    // console.log(country);
 
     const [visited,setVisited]= useState(false);
 
-    const handleVisited=()=>{
+    const handleVisited=()=>{  // state ke modify korar jonno event handler
         setVisited(!visited);   //toggle
     }
 
+    const passWithParams =()=>{
+        handleVisitedCountry(country)
+    }
+
     return (
-        <div className='country'>
-            <h3>Name: {name.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited? 'purple':'white'}} >Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
+            {/* event trigger */}
+            <button onClick={passWithParams}>Mark Visited</button>
+            <br />
             <button onClick={handleVisited}>{visited?'Visited': 'Lets go'}</button>
             {
                 visited ? 'I have visited this country': 'I wish to visit'
